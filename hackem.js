035eec95e402589f1cd58071665bd52d9e352a51
Aditya Mukherjee <aditya@adityamukherjee.com>
2011-04-11 07:04:39 -0700

    hardcoded 'hnuBase'

--- hackem.js
+++ hackem.js
@@ -8,6 +8,7 @@
 
 // Loadem Up! 
 window.bookmarklet = (function(opts){fullFunc(opts)})({
+    hnuBase: "https://github.com/adityavmr/HackemUp/raw/master/",
     css : [hnuBase + "hackemup.css"],
     js  : [
         hnuBase + "hackemup.js",
